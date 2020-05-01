93903831d3f83b662dfaaefd41d974a2a1a0269c
Rich <12820184+ris0@users.noreply.github.com>
2020-05-01 15:37:59 -0400

    Getting started (#2)
    * Complete hello_world exercise
    
    * Complete hello_cargo exercise; 1.2
    
    Co-authored-by: Rich Song <richard.song@hbc.com>

---
+++ GettingStarted/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/richardsong/code/learning-rust/GettingStarted/hello_cargo/target/debug/hello_cargo: /Users/richardsong/code/learning-rust/GettingStarted/hello_cargo/src/main.rs
---
+++ GettingStarted/hello_world/main.rs
@@ -0,0 +1,12 @@
+// defines a function
+fn main() {
+    println!("Hello, world!");
+}
+
+// rustc [filename] commands to compile the file
+// ./main runs the program
+
+// println! calls a rust macro. If it called a function instead, it would be entered as println without the !
+
+// after compiling successfully, Rust outputs a binary executable
+
